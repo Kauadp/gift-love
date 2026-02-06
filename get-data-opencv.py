77317ba2d598ffbf393651f4c3f371c2fe639ca2
kauadp <kauadp1405@gmail.com>
2026-02-06 12:08:07 -0300

    Scrapping Script 99% Finalizado

--- get-data-opencv.py
+++ get-data-opencv.py
@@ -7,6 +7,7 @@ import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 import time
+import pandas as pd
 
 
 # ===================== Funções Auxiliares =======================
@@ -14,13 +15,13 @@ import time
 
 # ----- Função para normalizar coordenadas em referência ao pulso --------
 
-def coords_norm(landmar_list):
-    x0 = landmar_list.landmark[0].x
-    y0 = landmar_list.landmark[0].y
-    z0 = landmar_list.landmark[0].z
+def coords_norm(landmark_list):
+    x0 = landmark_list[0].x
+    y0 = landmark_list[0].y
+    z0 = landmark_list[0].z
 
     temp_landmark_list = []
-    for lm in landmar_list:
+    for lm in landmark_list:
         temp_landmark_list.append(lm.x - x0)   
         temp_landmark_list.append(lm.y - y0)   
         temp_landmark_list.append(lm.z - z0)   
@@ -51,6 +52,11 @@ options = HandLandmarkerOptions(
 
 # ============== Configura Res e ademais utilidades do OpenCV ===================
 
+dataset = []
+modo_gravacao = None
+frames_restantes = 0
+contador = {"A":0, "B":0, "C":0, "D":0, "E":0}
+
 with HandLandmarker.create_from_options(options) as detector:
     camera = cv2.VideoCapture(0)
     camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
@@ -73,15 +79,14 @@ with HandLandmarker.create_from_options(options) as detector:
 
         # ---- Desenhar linhas e points do MediaPipe --------
 
-        coords_draw = None
+        coords_wrist = [] # Para normalizar em relação ao pulso
         if resultados.hand_landmarks:
             for hand_landmarks in resultados.hand_landmarks:
-                coords_draw = []
+                coords_wrist.extend(coords_norm(hand_landmarks)) # Para normalizar em relação ao pulso
                 for landmark in hand_landmarks:
                     x = int(landmark.x * imagem.shape[1])
                     y = int(landmark.y * imagem.shape[0])
                     cv2.circle(imagem, (x,y), 5, (0,0,255), -1)
-                    coords_draw.extend([landmark.x, landmark.y, landmark.z])
                 
                 for start_idx, end_idx in HAND_CONNECTIONS:
                     start = hand_landmarks[start_idx]
@@ -102,11 +107,43 @@ with HandLandmarker.create_from_options(options) as detector:
 
         cv2.putText(imagem, fps_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0))
 
+        # ----------- Grava Coords -------------
+
+        if modo_gravacao and coords_wrist is not None:
+            dataset.append(coords_wrist + [modo_gravacao])
+            contador[modo_gravacao] += 1
+            frames_restantes -= 1
+            if frames_restantes <= 0:
+                modo_gravacao = None
+
+        
+        for i, (gesto, total) in enumerate(contador.items()):
+            cv2.putText(imagem, f"{gesto}: {total}", (10, 60 + 30 * i),
+                        cv2.FONT_HERSHEY_SIMPLEX, .8, (0, 255, 255), 2)
+
+
+        # ----------- // -------------
+
         cv2.imshow("Test", imagem)
 
-        if cv2.waitKey(1) % 0xFF == ord("q"):
-            break
-        
+        # ---------- Keys Control ----------------
+        tecla = cv2.waitKey(1) % 0XFF
+
+        if tecla == ord("a"): modo_gravacao, frames_restantes = "A", 100
+        if tecla == ord("b"): modo_gravacao, frames_restantes = "B", 100
+        if tecla == ord("c"): modo_gravacao, frames_restantes = "C", 100
+        if tecla == ord("d"): modo_gravacao, frames_restantes = "D", 100
+        if tecla == ord("e"): modo_gravacao, frames_restantes = "E", 100
+
+        if tecla == 27: break
+
+# =========== Salva os Dados ===========
+
+df = pd.DataFrame(dataset)
+df.to_csv("data/coords_to_train.csv", index=False)
+
+# ========= // =============
+
 camera.release()
 cv2.destroyAllWindows()
 print("Tchau")
